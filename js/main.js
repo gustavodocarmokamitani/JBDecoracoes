7fc8ac41194b6acf1785ef1898d4bf930a82ef18
gustavodocarmokamitani <gustavodocarmokamitani@gmail.com>
2022-11-22 12:15:34 -0300

    teste

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+$('.carousel').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 1008,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },   
+  ],
+
+});
+
+
+
+AOS.init();
+
+console.log(window.screen.width)
